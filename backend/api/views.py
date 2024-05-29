03a9663455d1047a3ec9230eff0b450da5ca54e0
Pawel-Pioro <pavvcp3650@gmail.com>
2024-05-29 10:08:30 +0100

    added ai integration

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,8 +3,39 @@ from .serializers import CommitSerializer, ScenarioSerializer
 from .models import CommitMessage, Scenario
 from rest_framework.response import Response
 
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.common.keys import Keys 
+chrome_options = webdriver.ChromeOptions() 
+chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36') 
+chrome_options.add_argument('--headless') 
+chrome_options.add_argument('--disable-gpu') 
+chrome_options.add_argument('--incognito') 
+
+def getAiResponse(prompt): 
+    dr = webdriver.Chrome(options=chrome_options) 
+    WebDriverWait(dr, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body'))) 
+    dr.get("https://www.perplexity.ai/") 
+
+    WebDriverWait(dr, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "[placeholder^='Ask anything...']"))) 
+    inputElement = dr.find_element(By.CSS_SELECTOR, "[placeholder^='Ask anything...']") 
+    inputElement.send_keys(prompt) 
+    inputElement.send_keys(Keys.ENTER) 
+    WebDriverWait(dr, 40).until(EC.presence_of_element_located((By.XPATH, "//div[text()='Rewrite']"))) 
+
+    all_spans = [] 
+    notAllowed = [str(i) for i in range(10)] 
+        
+    for span in dr.find_elements(By.TAG_NAME, "span"): 
+        all_spans.append(span.text) 
+    dr.quit() 
+    return " ".join(all_spans[6:-7]) 
 
 def ai_function() -> dict:
+    aiResult = getAiResponse("Generate me a scenario and some code for it")
+    print(aiResult)
     result = {"scenario":"Some change has happened", "code":"import random"}
     return result
 
